235bad4fb271cb70535b89461b23202084910b8e
Robert Dumitrescu <robertdum01@gmail.com>
2016-12-29 03:59:12 +0000

    Modified the expected global object to match the new config implementation

--- generic.Logger.js
+++ generic.Logger.js
@@ -9,28 +9,28 @@ class GenericLogger {
     /** Generic logging*/
 
     static endpointTriggeredLog (endpoint) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.generic.genericLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.generic.genericLoggingOn){return;}
 
-        if (global.loggerSettings.generic.endpointLoggingOn) {
+        if (global.config.loggerSettings.generic.endpointLoggingOn) {
             console.log("[Controller][Endpoint]: " + endpoint);
         }
     }
 
     static granularMethodTriggeredLog (granularMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.generic.genericLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.generic.genericLoggingOn){return;}
 
-        if (global.loggerSettings.generic.granularLoggingOn) {
+        if (global.config.loggerSettings.generic.granularLoggingOn) {
             console.log("[Granular]: " + granularMethod);
         }
     }
 
     static databaseStatementLog (databaseStatement) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.generic.genericLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.generic.genericLoggingOn){return;}
 
-        if (global.loggerSettings.generic.databaseStatementLoggingOn) {
+        if (global.config.loggerSettings.generic.databaseStatementLoggingOn) {
             console.log("[DBS]: " + databaseStatement);
         }
     }
@@ -39,37 +39,37 @@ class GenericLogger {
     /** Component type - method triggered logging **/
 
     static controllerMethodTriggeredLog (controllerMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.controllerMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.controllerMethodTriggeredLogginOn) {
             console.log("[Controller]: " + controllerMethod);
         }
     }
 
     static serviceMethodTriggeredLog (serviceMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.serviceMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.serviceMethodTriggeredLogginOn) {
             console.log("[Service]: " + serviceMethod);
         }
     }
 
     static modelMethodTriggeredLog (modelMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.modelMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.modelMethodTriggeredLogginOn) {
             console.log("[Model]: " + modelMethod);
         }
     }
 
     static viewModelMapperMethodTriggeredLog (viewModelMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.viewModelMapperMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.viewModelMapperMethodTriggeredLogginOn) {
             console.log("[View.Model]: " + viewModelMethod);
         }
     }
@@ -79,47 +79,47 @@ class GenericLogger {
      * @param entityMapperMethod
      */
     static entityMapperMethodTriggeredLog (entityMapperMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.entityMapperMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.entityMapperMethodTriggeredLogginOn) {
             console.log("[Model]: " + entityMapperMethod);
         }
     }
 
 
     static entityModelMapperMethodTriggeredLog (entityModelMapperMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.entityModelMapperMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.entityModelMapperMethodTriggeredLogginOn) {
             console.log("[Entity.Model]: " + entityModelMapperMethod);
         }
     }
 
     static repositoryMethodTriggeredLog (repositoryMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.repositoryMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.repositoryMethodTriggeredLogginOn) {
             console.log("[Repository]: " + repositoryMethod);
         }
     }
 
     static validatorMethodTriggeredLog (validatorMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.validatorMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.validatorMethodTriggeredLogginOn) {
             console.log("[Validator]: " + validatorMethod);
         }
     }
 
     static helperMethodTriggeredLog (helperMethod) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.components.componentsLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.components.componentsLoggingOn){return;}
 
-        if (global.loggerSettings.components.helperMethodTriggeredLogginOn) {
+        if (global.config.loggerSettings.components.helperMethodTriggeredLogginOn) {
             console.log("[Helper]: " + helperMethod);
         }
     }
@@ -128,19 +128,19 @@ class GenericLogger {
 
 
     static errorPromiseObjectLog (errorPromiseObject) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.promises.promisesLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.promises.promisesLoggingOn){return;}
 
-        if (global.loggerSettings.promises.errorPromiseObjectLoggingOn) {
+        if (global.config.loggerSettings.promises.errorPromiseObjectLoggingOn) {
             console.log(errorPromiseObject);
         }
     }
 
     static resolvePromiseObjectLog (resolvePromiseObject) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.promises.promisesLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.promises.promisesLoggingOn){return;}
 
-        if (global.loggerSettings.promises.resolvePromiseObjectLoggingOn) {
+        if (global.config.loggerSettings.promises.resolvePromiseObjectLoggingOn) {
             console.log(resolvePromiseObject);
         }
     }
@@ -148,10 +148,10 @@ class GenericLogger {
     /** Flow related logging **/
 
     static responseObjectLog (responseObject) {
-        if(!global.loggerSettings.loggingOn){return;}
-        if(!global.loggerSettings.flow.flowLoggingOn){return;}
+        if(!global.config.loggerSettings.loggingOn){return;}
+        if(!global.config.loggerSettings.flow.flowLoggingOn){return;}
 
-        if (global.loggerSettings.flow.responseObjectLoggingOn) {
+        if (global.config.loggerSettings.flow.responseObjectLoggingOn) {
             console.log(responseObject);
         }
     }
